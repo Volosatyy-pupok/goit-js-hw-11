38e6075e0e43dfdcafe8910d49fc9aa8ef762adb
Volosatyy-pupok <derevyanko.2001.oleg@gmail.com>
2024-09-25 19:03:31 +0300

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,6 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 const API_KEY = '46141874-6e703aba01419e220f8d650c5';
 const BASE_URL = 'https://pixabay.com/api/';
 
@@ -5,18 +8,19 @@ export const fetchImages = query => {
   return fetch(
     `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
   )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(
-          `Error fetching images: ${response.status} ${response.statusText}`
-        );
-      }
-      return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
       if (data.hits.length === 0) {
-        throw new Error('No images found for this query.');
+        return [];
       }
       return data.hits;
+    })
+    .catch(() => {
+      iziToast.error({
+        title: 'Error!',
+        message: 'Failed to fetch images!',
+        position: 'topRight'
+      });
+      throw new Error('Fetch failed');
     });
 };
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { fetchImages } from './js/pixabay-api.js';
 import { renderImages, showLoader, hideLoader } from './js/render-functions.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const searchForm = document.querySelector('.search-form');
 const galleryElement = document.querySelector('.gallery');
@@ -20,6 +22,14 @@ searchForm.addEventListener('submit', event => {
   showLoader();
   fetchImages(query)
     .then(images => {
+      if (images.length === 0) {
+        iziToast.warning({
+          title: 'Sorry',
+          message: 'There are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
       renderImages(images);
       gallery.refresh();
     })
@@ -27,8 +37,8 @@ searchForm.addEventListener('submit', event => {
       console.error(error);
       iziToast.error({
         title: 'Error',
-        message:
-          'An error occurred while fetching images. Please try again later.',
+        message: 'An error occurred while fetching images. Please try again later.',
+        position: 'topRight',
       });
     })
     .finally(() => {
